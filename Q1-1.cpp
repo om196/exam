d4473df162d4df4c4713e6e0133aecdf74410dd0
om196 <ospatel196@gmail.com>
2023-06-02 11:46:17 +0930

    first commit

--- Q1-1.cpp
+++ Q1-1.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
 long product(int* arr, int n) {
-    if (n < 1) {
-        return -1;
+    if (n < 1) { // will work only if the size of the array is less than 1.
+        return -1; // Return -1 to indicate a invalid input.
     }
 
-    long solution = 1;
-    for (int a = 0; a < n; a++) {
-        solution *= arr[a];
+    long solution = 1; // Initialize a variable to store the product of the array elements.
+    for (int a = 0; a < n; a++) { 
+        solution *= arr[a]; // will Multiply the current element with the product.
     }
 
-    return solution;
+    return solution; // Return the final answer
 }
-
-
 
--- Q1-2.cpp
+++ Q1-2.cpp
@@ -1,13 +1,14 @@
+
 int countOddEven(int** matrix, int nRows, int nCols, bool even) {
-    int total = 0;
-    for (int a = 0; a < nRows; a++) {
-        for (int b = 0; b < nCols; b++) {
-            if (even && matrix[a][b] % 2 == 0) {
-                total++;
-            } else if (!even && matrix[a][b] % 2 != 0) {
-                total++;
+    int total = 0; // will start with a variable to track the count.
+    for (int a = 0; a < nRows; a++) { // Loop through each row of the matrix
+        for (int b = 0; b < nCols; b++) { // Loop through each column of the matrix
+            if (even && matrix[a][b] % 2 == 0) { // If "even" is true and the matrix element is even
+                total++; // add in the count
+            } else if (!even && matrix[a][b] % 2 != 0) { // If "even" is false than the matrix element is odd
+                total++; // add in the count 
             }
         }
     }
-    return total;
+    return total; // Return the total count
 }
